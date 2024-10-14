dab8e724833805a646a7c70703cb27d890e151e9
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-10-14 20:20:19 +0800

    Created Model and Controller

---
+++ SuperHeroAPI-DotNet/SuperHeroAPI-DotNet/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_DotNet.Models;
+
+namespace SuperHeroAPI_DotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham City"
+                }
+            };
+           return Ok(heroes);
+        }
+    }
+}
